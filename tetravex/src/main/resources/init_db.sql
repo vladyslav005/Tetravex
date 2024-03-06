2362f9bc954589d3cb04bfcacc346f3b43deb78a
Vladyslav Futrak <vladyslav.futrak@student.tuke.sk>
2024-03-06 18:25:44 +0100

    edit sql script

--- tetravex/src/main/resources/init_db.sql
+++ tetravex/src/main/resources/init_db.sql
@@ -2,14 +2,14 @@ CREATE TABLE Score (
     player VARCHAR(64),
     game VARCHAR(64),
     points INTEGER,
-    playedOn DATE
+    playedOn TIMESTAMP
 );
 
 CREATE TABLE Rating (
     player VARCHAR(64),
     game  VARCHAR(64),
     rating  INTEGER,
-    ratedOn DATE,
+    ratedOn TIMESTAMP,
     PRIMARY KEY (player, game)
 );
 
@@ -17,5 +17,5 @@ CREATE TABLE Comment (
      player VARCHAR(64),
      game VARCHAR(64),
      comment VARCHAR(64),
-     commentedOn DATE
+     commentedOn TIMESTAMP
 );
